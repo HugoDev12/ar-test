3c747ce2971186dcdf5e3b41c3bde908b87a2a8c
Occelli <occelli.hugo@hotmail.fr>
2024-09-12 23:35:44 +0200

    update on buttons.js to get my iphone current localisation latitude and longitude

--- js/buttons.js
+++ js/buttons.js
@@ -15,9 +15,31 @@ document.getElementById('starts').addEventListener('click', function () {
     const ringContainer = document.getElementById('ring-container');
 
     // Créer les anneaux et les ajouter à la scène AR
-    ringContainer.innerHTML = `
-<a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 0 0" radius="1.5" radius-tubular="0.1" color="#FF5733"></a-torus>
-<a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 1 0" radius="2" radius-tubular="0.1" color="#33FF57"></a-torus>
-<a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 2 0" radius="2.5" radius-tubular="0.1" color="#3357FF"></a-torus>
-`;
+    //     ringContainer.innerHTML = `
+    // <a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 0 0" radius="1.5" radius-tubular="0.1" color="#FF5733"></a-torus>
+    // <a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 1 0" radius="2" radius-tubular="0.1" color="#33FF57"></a-torus>
+    // <a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 2 0" radius="2.5" radius-tubular="0.1" color="#3357FF"></a-torus>
+    // `;
+    // });
+
+
+    // Vérifier si le navigateur fournit bien les données GPS
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+
+            // Ajouter les anneaux au conteneur basé sur les coordonnées GPS
+            ringContainer.innerHTML = `
+            <a-torus gps-entity-place="latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}" position="0 0 0" radius="1.5" radius-tubular="0.1" color="#FF5733"></a-torus>
+            <a-torus gps-entity-place="latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}" position="0 1 0" radius="2" radius-tubular="0.1" color="#33FF57"></a-torus>
+            <a-torus gps-entity-place="latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}" position="0 2 0" radius="2.5" radius-tubular="0.1" color="#3357FF"></a-torus>
+          `;
+        }, function (error) {
+            console.error("Erreur lors de la récupération des données GPS: ", error);
+            alert("Impossible de récupérer les données GPS.");
+        });
+    } else {
+        console.error("La géolocalisation n'est pas supportée par ce navigateur.");
+        alert("La géolocalisation n'est pas supportée.");
+    }
 });
