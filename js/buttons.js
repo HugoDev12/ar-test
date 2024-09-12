d8d54e8287752aae29f20f8427c7d21df527f618
Occelli <occelli.hugo@hotmail.fr>
2024-09-12 23:48:51 +0200

    update on buttons.js to get my iphone current localisation latitude and longitude2

--- js/buttons.js
+++ js/buttons.js
@@ -14,15 +14,6 @@
 document.getElementById('starts').addEventListener('click', function () {
     const ringContainer = document.getElementById('ring-container');
 
-    // Créer les anneaux et les ajouter à la scène AR
-    //     ringContainer.innerHTML = `
-    // <a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 0 0" radius="1.5" radius-tubular="0.1" color="#FF5733"></a-torus>
-    // <a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 1 0" radius="2" radius-tubular="0.1" color="#33FF57"></a-torus>
-    // <a-torus gps-entity-place="latitude: 49.443233; longitude: 1.099971" position="0 2 0" radius="2.5" radius-tubular="0.1" color="#3357FF"></a-torus>
-    // `;
-    // });
-
-
     // Vérifier si le navigateur fournit bien les données GPS
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
